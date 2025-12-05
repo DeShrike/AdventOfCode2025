19c39a06bf0866ca9d5686cfe408b8d2f45c9d02
Shrike <de_shrike@hotmail.com>
2025-12-05 17:27:26 +0100

    Day 5 (WIP)

---
+++ src/day5.py
@@ -0,0 +1,115 @@
+from aoc import Aoc
+import itertools
+import math
+import re
+import sys
+
+# Day 5
+# https://adventofcode.com/2025
+
+class Day5Solution(Aoc):
+
+   def Run(self):
+      self.StartDay(5, "AOC")
+      self.ReadInput()
+      self.PartA()
+      self.PartB()
+
+   def Test(self):
+      self.StartDay(5)
+
+      goal = self.TestDataA()
+      self.PartA()
+      self.Assert(self.GetAnswerA(), goal)
+
+      goal = self.TestDataB()
+      self.PartB()
+      self.Assert(self.GetAnswerB(), goal)
+
+   def TestDataA(self):
+      self.inputdata.clear()
+      testdata = \
+      """
+      3-5
+      10-14
+      16-20
+      12-18
+
+      1
+      5
+      8
+      11
+      17
+      32
+      """
+      self.inputdata = [line.strip() for line in testdata.strip().split("\n")]
+      return 3
+
+   def TestDataB(self):
+      self.inputdata.clear()
+      # self.TestDataA()    # If test data is same as test data for part A
+      testdata = \
+      """
+      1000
+      2000
+      3000
+      """
+      self.inputdata = [line.strip() for line in testdata.strip().split("\n")]
+      return None
+
+   def ParseInput(self):
+      # rx = re.compile("^(?P<from>[A-Z0-9]{3}) = \((?P<left>[A-Z0-9]{3}), (?P<right>[A-Z0-9]{3})\)$")
+      # match = rx.search(line)
+      # pos = match["from"]
+
+      data = []
+      for line in self.inputdata:
+         data.append(line)
+
+      return data
+
+   def PartA(self):
+      self.StartPartA()
+
+      data = self.ParseInput()
+      answer = 0
+
+      ranges = []
+      ids = []
+      for l, lijn in enumerate(data):
+         if "-" in lijn:
+            parts = lijn.split("-")
+            ranges.append(( int(parts[0]), int(parts[1]) ))
+         elif lijn == "":
+            continue
+         else:
+            ids.append(int(lijn))
+
+      for ID in ids:
+         for r in ranges:
+            if r[0] <= ID <= r[1]:
+               answer += 1
+               break
+
+      self.ShowAnswer(answer)
+
+   def PartB(self):
+      self.StartPartB()
+
+      data = self.ParseInput()
+      answer = None
+
+      # Add solution here
+
+      self.ShowAnswer(answer)
+
+
+if __name__ == "__main__":
+   solution = Day5Solution()
+   if len(sys.argv) >= 2 and sys.argv[1] == "test":
+      solution.Test()
+   else:
+      solution.Run()
+
+# Template Version 1.5
+
