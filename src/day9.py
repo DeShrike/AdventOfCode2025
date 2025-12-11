f22051a5df64f8e75924bd52fc56a30112e01cd4
Shrike <de_shrike@hotmail.com>
2025-12-11 23:40:00 +0100

    More progress with Day 9 Part 2

--- src/day9.py
+++ src/day9.py
@@ -1,4 +1,6 @@
 from aoc import Aoc
+from canvas import Canvas
+from utilities import neighbours8
 import itertools
 import math
 import re
@@ -6,6 +8,16 @@ import sys
 
 # Day 9
 # https://adventofcode.com/2025
+
+class Point():
+   def __init__(self, x: int, y: int) -> None:
+      self.x = x
+      self.ox = x
+      self.y = y
+      self.oy = y
+
+   def __repr__(self):
+      return f"Point({self.x},{self.y})"
 
 class Day9Solution(Aoc):
 
@@ -73,7 +85,6 @@ class Day9Solution(Aoc):
 
       self.ShowAnswer(answer)
 
-   """
    def IsInRect(self, p1, p2, p) -> bool:
       pp1 = (min(p1[0], p2[0]), min(p1[1], p2[1]))
       pp2 = (max(p1[0], p2[0]), max(p1[1], p2[1]))
@@ -89,163 +100,41 @@ class Day9Solution(Aoc):
          b, a = a, b
       return a < x < b;
 
-   def IsOnBorder(self, points, p) -> bool:
-      for ix, p1 in enumerate(points):
-         np = points[(ix + 1) % len(points)]
+   def GridToPng(self, grid) -> None:
+      width = len(grid[0])
+      height = len(grid)
+      boxsize = 5
 
-         if p1[0] == np[0]:
-            # vertical line
-            if p[0] == p1[0]:
-               if self.IsBetweenIncl(p1[1], np[1], p[1]):
-                  return True
-         elif p1[1] == np[1]:
-            # horizontal line
-            if p[1] == p1[1]:
-               if self.IsBetweenIncl(p1[0], np[0], p[0]):
-                  return True
-         else:
-            print("Oops")
-            aa = input
-      return False
+      canvas = Canvas(width * boxsize, height * boxsize)
+      for y, row in enumerate(grid):
+         for x, col in enumerate(row):
+            if col == 1:
+               canvas.set_big_pixel(x * boxsize, y * boxsize, (255,0,0), boxsize)
+            elif col == 2:
+               canvas.set_big_pixel(x * boxsize, y * boxsize, (0,255,0), boxsize)
+            elif col == 3:
+               canvas.set_big_pixel(x * boxsize, y * boxsize, (0,0,255), boxsize)
 
-   def IsInPolygon(self, points, p) -> bool:
-      i = 90
-      j = len(points) - 1
-      oddNodes=False
-      polyY = [pp[1] for pp in points]
-      polyX = [pp[0] for pp in points]
-      x, y = p
-      for i in range(len(points)):
-         if (polyY[i] < y and polyY[j] >= y or polyY[j] < y and polyY[i] >= y):
-            if (polyX[i] + (y - polyY[i]) / (polyY[j] - polyY[i]) * (polyX[j] - polyX[i]) < x):
-               oddNodes = not oddNodes
-         j = i 
-      return oddNodes or self.IsOnBorder(points, p)
+      pngname = "day9b.png"
+      print(f"Saving {pngname}")
+      canvas.save_PNG(pngname)
 
-   def IsInPolygonX(self, points, p) -> bool:
-      vertical_lines = []
-      for ix, p1 in enumerate(points):
-         np = points[(ix + 1) % len(points)]
-         if p1[0] == np[0]:
-            if p1[1] < np[1]:
-               vertical_lines.append((p1, np))
-            else:
-               vertical_lines.append((np, p1))
-      #print("vertical lines:")
-      #print(vertical_lines)
-      #a = input()
-      print("IsInPolygon", p)
-      vlines = [True for l in vertical_lines if p[0] < l[0][0] and l[0][1] <= p[1] <= l[1][1]]
-      print("vlines:", len(vlines))
-      print(vlines)
-      bb = input()
-      return (len(vlines) % 2 == 1) or self.IsOnBorder(points, p)
-
-   def IsRectInPolygon(self, p1, p2, points) -> bool:
-      pp1 = (min(p1[0], p2[0]), min(p1[1], p2[1]))
-      pp2 = (max(p1[0], p2[0]), max(p1[1], p2[1]))
-      tr = (pp2[0], pp1[1])
-      bl = (pp1[0], pp2[1])
-
-      return self.IsInPolygon(points, tr) and self.IsInPolygon(points, bl)
-   """
-
-   def IsRectInPolygon(self, p1, p2, points, lines) -> bool:
-      pp1 = (min(p1[0], p2[0]), min(p1[1], p2[1]))
-      pp2 = (max(p1[0], p2[0]), max(p1[1], p2[1]))
-      tr = (pp2[0], pp1[1])
-      bl = (pp1[0], pp2[1])
-
-      #print("Trying")
-      #print("TL: ", pp1)
-      #print("TR: ", tr)
-      #print("BL: ", bl)
-      #print("BR: ", pp2)
-
-      for line in lines:
-         if line[0] == p1 or line[0] == p2 or line[1] == p1 or line[1] == p2:
-            continue
-
-         if self.DoLinesIntersect(pp1, tr, line[0], line[1]):
-            return False
-         if self.DoLinesIntersect(tr, pp2, line[0], line[1]):
-            return False
-         if self.DoLinesIntersect(bl, pp2, line[0], line[1]):
-            return False
-         if self.DoLinesIntersect(pp1, bl, line[0], line[1]):
-            return False
-
-      #print("YES")
-      #aa = input()
-
-      return True
-
-   def DoLinesIntersect(self, l1a, l1b, l2a, l2b) -> bool:
-      """
-      line 1: l1a to l1b
-      line 2: l2a to l2b
-      """
-      #print("Intersect ? ", l1a, "to", l1b, " and ", l2a, l2b)
-
-      # if both vertical then return false
-      if l1a[0] == l1b[0] and l2a[0] == l2b[0]:
-         #print("both vertical")
-         return False
-
-      # if both horizontal then return false
-      if l1a[1] == l1b[1] and l2a[1] == l2b[1]:
-         #print("both horizontal")
-         return False
-
-      if l1a[0] == l1b[0]:
-         va = l1a
-         vb = l1b
-         ha = l2a
-         hb = l2b
-      else:
-         ha = l1a
-         hb = l1b
-         va = l2a
-         vb = l2b
-
-      va = (va[0], va[1] - 0.5)
-      vb = (vb[0], vb[1] + 0.5)
-      ha = (ha[0] - 0.5, ha[1])
-      hb = (hb[0] + 0.5, hb[1])
-
-      #print("hor: ", ha, hb)
-      #print("ver: ", va, vb)
-
-      if ha[0] < va[0] and hb[0] > va[0] and \
-         va[1] < ha[1] and vb[1] > ha[1]:
-         #print("intersect")
-         return True
-
-      #print("no intersect")
-
-      return False
-
-   def CalcLines(self, points):
-      lines = []
-      for ix, p1 in enumerate(points):
-         np = points[(ix + 1) % len(points)]
-         if p1[0] == np[0]:
-            # vertical
-            if p1[1] < np[1]:
-               lines.append((p1, np))
-            else:
-               lines.append((np, p1))
-         elif p1[1] == np[1]:
-            # horizontal
-            if p1[0] < np[0]:
-               lines.append((p1, np))
-            else:
-               lines.append((np, p1))
-         else:
-            print("OOOOppps")
-            aa = input()
-
-      return lines
+   def Floodfill(self, grid) -> None:
+      width = len(grid[0])
+      height = len(grid)
+      x = width // 2
+      y = height // 4
+      grid[y][x] = 3
+      print("floodfilling")
+      q = [(x, y)]
+      while len(q) > 0:
+         p = q.pop()
+         grid[p[1]][p[0]] = 3
+         for n in neighbours8(p[0], p[1], (width, height)):
+            if grid[n[1]][n[0]] == 0:
+               grid[n[1]][n[0]] = 3
+               q.append((n[0], n[1]))
+      print("done")
 
    def PartB(self):
       self.StartPartB()
@@ -253,61 +142,77 @@ class Day9Solution(Aoc):
       data = self.ParseInput()
       answer = 0
 
-      points = [ (int(l[0]), int(l[1])) for l in [ (line.split(",")) for line in data ] ]
-      lines = self.CalcLines(points)
+      points = [ Point(int(l[0]), int(l[1])) for l in [ (line.split(",")) for line in data ] ]
 
-      """
-      minx = 1_000_000
-      miny = 1_000_000
-      maxx = 0
-      maxy = 0
+      minx = miny = 1_000_000_000
+      maxx = maxy = 0
+      for point in points:
+         minx = min(minx, point.x)
+         miny = min(miny, point.y)
+         maxx = max(maxx, point.x)
+         maxy = max(maxy, point.y)
+
+      print(f"Extent X: {minx} -> {maxx}")
+      print(f"Extent Y: {miny} -> {maxy}")
+
+      xs = list(set([p.x for p in points]))
+      xs.sort()
+      ys = list(set([p.y for p in points]))
+      ys.sort()
+
+      ny = 1
+      for y in ys:
+         ny += 1
+         for p in points:
+            if p.y == y:
+               p.y = ny
+      nx = 1
+      for x in xs:
+         nx += 1
+         for p in points:
+            if p.x == x:
+               p.x = nx
+
+      minx = miny = 1_000_000_000
+      maxx = maxy = 0
+      for point in points:
+         minx = min(minx, point.x)
+         miny = min(miny, point.y)
+         maxx = max(maxx, point.x)
+         maxy = max(maxy, point.y)
+
+      print(f"Extent X: {minx} -> {maxx}")
+      print(f"Extent Y: {miny} -> {maxy}")
+
+      grid = [[0 for x in range(maxx + 5)] for y in range(maxy + 5)]
+
+      for ix, p1 in enumerate(points):
+         nx = (ix + 1) % len(points)
+         p2 = points[nx]
+         if p1.x == p2.x:
+            # vert
+            y1 = p1.y
+            y2 = p2.y
+            if y1 > y2:
+               y1, y2 = y2, y1
+            for y in range(y1, y2):
+               grid[y][p1.x] = 2
+         else:
+            # hor
+            x1 = p1.x
+            x2 = p2.x
+            if x1 > x2:
+               x1, x2 = x2, x1
+            for x in range(x1, x2):
+               grid[p1.y][x] = 2
+
       for p in points:
-         minx = min(minx, p[0])
-         maxx = max(maxx, p[0])
-         miny = min(miny, p[1])
-         maxy = max(maxy, p[1])
-      print(f"X : {minx} -> {maxx}")
-      print(f"Y : {miny} -> {maxy}")
+         grid[p.y][p.x] = 1
 
-      for y in range(maxy + 2):
-         for x in range(maxx + 2):
-            print(f"({x}, {y})", "Yes" if self.IsInPolygon(points, (x, y) ) else "")
-            a = input()
-      """
+      self.Floodfill(grid)
 
-      """
-      for a, p1 in enumerate(points):
-         for b, p2 in enumerate(points):
-            if a >= b:
-               continue
-            area = self.CalcArea(p1, p2)
-            if area > answer:
-               if not self.IsRectInPolygon(p1, p2, points):
-                  continue
-               good = True
-               for c, p3 in enumerate(points):
-                  if a == c or b == c:
-                     continue
-                  if self.IsInRect(p1, p2, p3):
-                     good = False
-                     break
-               if good:
-                  print(p1, p2)
-                  answer = area
-      """
-
-      for a, p1 in enumerate(points):
-         for b, p2 in enumerate(points):
-            if a >= b:
-               continue
-            area = self.CalcArea(p1, p2)
-            if area > answer:
-               #print(p1, p2)
-               #aa = input()
-               if not self.IsRectInPolygon(p1, p2, points, lines):
-                  continue
-               answer = area
-
+      self.GridToPng(grid)
+      
       # Attempt 1: 4750092396 is too high
       # Attempt 2: 2679903198 is too high
       # Attempt 3: 1363080015 is too low
